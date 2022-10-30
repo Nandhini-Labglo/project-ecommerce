342d531ddacad397ab1cb5339121cb02a2bd7d1e
Nandhini-Labglo <nandhini.labglo@gmail.com>
2022-10-30 11:47:48 +0530

    check

--- website/urls.py
+++ website/urls.py
@@ -4,17 +4,15 @@ from django.urls import include, re_path as url
 
 urlpatterns = [
 
-    path('login/',views.login_request,name='login'),
-    path('logout/',views.logout_request,name='logout'),
     path('index/',views.index,name='index'),
     path('view_p/',views.view,name='view_p'),
     path('search/',views.SearchView.as_view(),name='search'),
     path('cart/',views.cart_detail,name='cart'),
     url(r'^add/(?P<id>\d+)/$', views.add_to_cart, name='add_to_cart'),
     path('remove_cart/<id>',views.remove_cart,name='remove_cart'),
-    path('orderplaced/',views.orderplaced,name='orderplaced'),
-    path('cancel_order/',views.cancel_order,name='cancel_order'),
     path('remove_item_cart/<id>', views.remove_item_cart, name='remove_item_cart'),
+    path('orderplaced/',views.orderplaced,name='orderplaced'),
+    path('cancel_order/<id>',views.cancel_order,name='cancel_order'),
     url(r'^add_wishlist/(?P<id>\d+)/$', views.add_to_wishlist, name='add_to_wishlist'),
     path('view_wishlist/',views.view_wishlist,name='wishlist'),
     path('remove_wishlist/<id>',views.remove_from_wishlist,name='remove_wish'),
